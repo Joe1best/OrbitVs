6c6f6d48601b5023b3cbfc8545ea5c7d9c724193
Youssef <joebestavros@live.com>
2020-05-05 15:57:17 -0400

    Changed up a lot of the logic that goes into calculating the orbits

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from random import random
 
-G = 6.674*10**(-11)
-
+#G = 6.674*10**(-11)
+G = 1
 def magnitude(v1):
     """
     Calculates the magnitude of v1 (vector)
@@ -16,40 +17,39 @@ def vectorBtw(pos1,pos2):
     return [pos2[0]-pos1[0],pos2[1]-pos1[1]]
 
 class Planet:
-    def __init__(self,mass,ecc,r0,star,radius,v0):
-        self.mass = mass
-        self.r0 = r0
-        self.v0 = v0
-        self.star = star
-        self.l = self.mass*self.r0**2*(self.v0/self/r0)
-        self.radius = radius
-        self.ecc = self.eccen(self.totalEnergy(),self.l,self.mass,G*self.star.mass)
 
+    def __init__(self,typeOfOrbit,star,frequency):
+	self.typeOfOrbit = typeOfOrbit
+        self.star = star
+        self.frequency = frequency
+	if (typeOfOrbit == 'circular'):
+		self.eccen = 0
+	elif (typeOfOrbit == 'elliptical'):
+		self.eccen = random()
+	self.r = self.findR()
+	
+    def findR(self):
+	"""
+	Function that calculates the radius given the type of orbit. This method only 
+	accepts circular and elliptical type at the moment. If the orbit is elliptical
+	it returns two parameters; the semi-major axis and the semi-minor axis.
+	"""
+
+	T = 1/self.frequency
+	if (self.typeOfOrbit == 'circular'):
+		return (T**2*G*self.star.mass/4*np.pi**2)**(1/3)
+	elif (self.typeOfOrbit == 'elliptical'):
+		return [(T**2*G*self.star.mass/4*np.pi**2)**(1/3), np.sqrt(a**2*(1-self.ecen**2))]
+		
     def totalEnergy(self):
         return (1/2)*self.mass*self.v0**2 - G*self.star.mass*self.mass/self.r0
 
-    def orbit(self,dtheta,l,m,u,E):   
+    def orbit(self,t):   
         """
-        Returns the orbit as a function of theta depending on the physical 
-        parameters of the planet
-        Input: dtheta (float): true anomaly of the system
-               m (float): mass of the body 
-               u: G*M (float) where G is the gravitational constant and M is the mass of the star
-               E: total energy of the system
+        Returns the orbit position as a function of time
         """     
-        c = l**2/(m**2*u)
-        e = self.ecc
-        return c*(1/(1+e*np.cos(dtheta)))
-
-    def eccen(self,E,l,m,u):
-        """
-        Returns the eccentricity of the planet 
-        Input: l (float) angular momentum of the body
-               m (float) mass of the body 
-               u: G*M (float) where G is the gravitational constant and M is the mass of the star
-        """
-        return np.sqrt(1+(2*E*l**2/(m**3*u**2)))
-
+	return [self.r*np.cos(2*np.pi*self.frequency*t), self.r*np.sin(2*np.pi*self.frequency*t)]
+	
 class Star: 
     def __init__(self,mass,radius):
         self.mass = mass
@@ -63,3 +63,14 @@ class system:
         """
         self.planets = [Planet(planet[i][0],planet[i][1],planet[i][2],planet[i][3],planet[i][4],planet[i][5]) for i in range(len(planet))]
         self.star = Star(star[0],star[1])
+s = Star(1,1)
+b = Planet('circular',s,2)
+
+x=[]
+y=[]
+t = np.linspace(0,1/2)
+for time in t:
+	x.append(b.orbit(time)[0])
+	y.append(b.orbit(time)[1])
+plt.plot(x,y)	
+plt.show()
