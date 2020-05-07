60f505dd156136967374185918c7c69167710d32
Joe1best <Joe1best@users.noreply.github.com>
2020-05-07 14:41:50 -0400

    Split files to make it more modular. Added a solar system option in system. Added options to save animation or take a snapshot instead

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random import random
 import matplotlib.animation as an 
-
+import starSystems as st
 
 #G = 6.674*10**(-11)
 G = 1
@@ -17,74 +17,16 @@ def vectorBtw(pos1,pos2):
     Calculates and returns the vector between object one and object two. 
     """
     return [pos2[0]-pos1[0],pos2[1]-pos1[1]]
-
-class Planet:
-    def __init__(self,typeOfOrbit,star,frequency,initCond,name,eccen=None):
-        self.typeOfOrbit = typeOfOrbit
-        self.star = star
-        self.frequency = frequency
-        self.name = name
-        if (typeOfOrbit == 'circular'):
-            self.eccen = 0
-        elif (typeOfOrbit == 'elliptical'):
-            if (eccen==None):
-                self.eccen = random()
-            else: 
-                self.eccen = eccen
-        self.r = self.findR()
-        self.initCond = initCond
-    
-    def findR(self):
-        """
-        Function that calculates the radius given the type of orbit. This method only 
-        accepts circular and elliptical type at the moment. If the orbit is elliptical
-        it returns two parameters; the semi-major axis and the semi-minor axis.
-        """
-        T = 1/self.frequency
-        if (self.typeOfOrbit == 'circular'):
-            return (T**2*G*self.star.mass/4*np.pi**2)**(1/3)
-        elif (self.typeOfOrbit == 'elliptical'):
-            return [(T**2*G*self.star.mass/4*np.pi**2)**(1/3), np.sqrt((T**2*G*self.star.mass/4*np.pi**2)**(2/3)*(1-self.eccen**2))]
-        return [0,0]
-
-    def orbit(self,t):   
-        """
-        Returns the orbit position as a function of time
-        """ 
-        if (self.typeOfOrbit == 'circular'):
-            return [self.r*np.cos(2*np.pi*self.frequency*t+self.initCond[0]), self.r*np.sin(2*np.pi*self.frequency*t+self.initCond[1])]
-        elif (self.typeOfOrbit == 'elliptical'):
-            return [self.r[0]*(1-self.eccen**2)/(1+self.eccen*np.cos(2*np.pi*self.frequency*t+self.initCond[0]))*np.cos(2*np.pi*self.frequency*t+self.initCond[0]),
-                            self.r[0]*(1-self.eccen**2)/(1+self.eccen*np.cos(2*np.pi*self.frequency*t+self.initCond[1]))*np.sin(2*np.pi*self.frequency*t+self.initCond[1])]
-    def drawOrbit(self):
-        time = np.linspace(0,1/self.frequency,300)
-        xdata, ydata = [], []
-        for t in time: 
-            xdata.append(self.orbit(t)[0])
-            ydata.append(self.orbit(t)[1])
-        return xdata,ydata
-
-
-
-class Star: 
-    def __init__(self,mass,radius):
-        self.mass = mass
-        self.radius = radius
-
-class System: 
-    def __init__(self,planets,star):
-        """
-        Input: Array of planet specifications (planet)
-               Array of star specifications (star)
-        """
-        self.planets = planets
-        self.star = star
     
 class Plotting:
-    def __init__(self,starSystem,nsteps,duration,connectingPlanets = None,saveFile=None):
+    def __init__(self,starSystem,nsteps,duration,connectingPlanets = None,saveFile=True,animationSave= False,ignorePlanet=None):
         #Declaring some variables
         self.ss = starSystem
-
+        
+        #Ignoring the planets that we want to ignore in the animation 
+        newPlanets = self.ss.planets.copy()
+        [newPlanets.remove(ignorePlanet[i]) for i in range(len(ignorePlanet))]
+        
         #Number of steps in the simulation
         self.nsteps = nsteps
 
@@ -93,7 +35,7 @@ class Plotting:
 
         #Parameters needed to calculate the length of the x and y axis 
         #for the animations
-        par = self.findMaxMin()
+        par = self.findMaxMin(newPlanets)
         
         #Setting the limits 
         self.limits = [min(par[0,:]), min(par[1,:]), max(par[2,:]), max(par[3,:])]
@@ -122,11 +64,13 @@ class Plotting:
         #Title depending on the purpose of the plot
         if (connectingPlanets==None):
             plt.title("Random Solar System",fontsize=25)
+        elif (ignorePlanet!=None):
+            plt.title(connectingPlanets[0].name+ " and "+ connectingPlanets[1].name+" Orbit Closeup",fontsize=25)
         else:
             plt.title(connectingPlanets[0].name+ " and "+connectingPlanets[1].name+" orbit",fontsize=25)
         
         #The object in our plot that will be animated
-        ptcls = [ax.plot([],[],'.',markersize=20,color=self.colors[_],markeredgecolor='black',zorder=10)[0] for _ in range(len(self.ss.planets))]
+        ptcls = [ax.plot([],[],'.',markersize=20,color=self.colors[_],markeredgecolor='black',zorder=10)[0] for _ in range(len(newPlanets))]
 
         t = np.linspace(0,self.duration,self.nsteps)
     
@@ -135,55 +79,58 @@ class Plotting:
                 ptcl.set_data([], [])
             return ptcls
         
+        #Will save all the data in there
+        data = np.zeros([len(connectingPlanets),2])
+
         def animate(i):
             #Updates the figure with the new points for each planet
-            for (p,ptcl) in zip(self.ss.planets,ptcls): 
+            
+            for (p,ptcl) in zip(newPlanets,ptcls): 
                 xdata = p.orbit(t[i])[0] 
                 ydata = p.orbit(t[i])[1] 
                 ptcl.set_data(xdata,ydata)
 
             if (connectingPlanets != None):
-                #gets all the current data on the graph 
-                data = np.zeros([len(self.ss.planets),2])
-                for (i,ptcl) in zip(range(len(self.ss.planets)),ptcls):
-                    data[i][0] = ptcl.get_xdata()
-                    data[i][1] = ptcl.get_ydata()
-
+                
                 #Figures out which index the planets that we want to connect are in 
                 #the data array 
                 index = []
-                for i in range(len(self.ss.planets)):
-                    if (self.ss.planets[i]==connectingPlanets[0] or self.ss.planets[i] == connectingPlanets[1]):
-                        index.append(i)
+                for cp in connectingPlanets:
+                    index.append(self.ss.planets.index(cp))
+                
+                #gets all the current data on the graph for the two planets that we want to connect
+                for (p,j,m) in zip(connectingPlanets,index,range(len(connectingPlanets))):
+                    data[m][0] = self.ss.planets[j].orbit(t[i])[0]
+                    data[m][1] = self.ss.planets[j].orbit(t[i])[1]
 
                 #Gives us the line equation parameters
-                a,b = self.connectTwoPlanets(data[index[0]],data[index[1]],t[i])
-                
-                minx = min([data[index[0]][0],data[index[1]][0]])
-                maxx = max([data[index[0]][0],data[index[1]][0]])
+                a,b = self.connectTwoPlanets(data[0],data[1])
+                minx = min([data[0][0],data[1][0]])
+                maxx = max([data[0][0],data[1][0]])
                 x = np.linspace(minx,maxx,100,endpoint=True)
                 plt.plot(x,a*x+b,'-',color='black')
             
-
             return ptcls
+        
         ani = an.FuncAnimation(fig,animate,init_func = init,interval=1,frames=nsteps,blit=True)
-         #If we do not want to save, just show the animation
-        if (saveFile!=None):
+        
+        #If we do not want to save, just show the animation
+        if (saveFile):
             #Saves the animation
-            ani.save(save+".gif",writer="pillow")
+            save = connectingPlanets[0].name + "-" + connectingPlanets[1].name
+            if (animationSave):
+                ani.save(save+".gif",writer="pillow")
 
-            #Saves the last snapshot of the figure
+            #Saves the last snapshot of the figure as an svg 
             fig.savefig(save+".svg", format='svg', dpi=1200)
-            fig.savefig(save+".png", format='png', dpi=1200)
-
             plt.close()
         else:
             plt.show()
         
-    def findMaxMin(self):
-        limits = np.zeros([4,len(self.ss.planets)])
+    def findMaxMin(self,newPlanets):
+        limits = np.zeros([4,len(newPlanets)])
         i = 0 
-        for p in self.ss.planets:
+        for p in newPlanets:
             x,y = p.drawOrbit()
             limits[0][i] = min(x)
             limits[1][i] = min(y)
@@ -192,7 +139,7 @@ class Plotting:
             i+=1
         return limits
     
-    def connectTwoPlanets(self,p1,p2,t):
+    def connectTwoPlanets(self,p1,p2):
         """
         Function that will draw a line between two given planet coordinates p1 and p2 at time t
         returns the parameters (a,b) in the line equation ax+b that passes through both points
@@ -203,27 +150,6 @@ class Plotting:
         return a,b
 
 
-
-
-s = Star(1,1)
-#plt.ion()
-
-numPlanets = 10
-typeOforbit = ['circular', 'elliptical']
-#Frequencies of planets relative to Earth (which is 1)
-freq1 = 4.15   #Mercury
-freq2 = 1.62   #Venus
-freq3 = 1      #Earth
-freq4 = 0.53   #Mars
-freq5 = 0.084  #Jupiter
-freq6 = 0.0344 #Saturn
-#Eccentricity values were googled and inputted here. 
-a = Planet(typeOforbit[1],s,freq1,[0,0],"Mercury",eccen=0.206)
-b = Planet(typeOforbit[1],s,freq2,[0,0],"Venus",eccen=0.007)
-c = Planet(typeOforbit[1],s,freq3,[0,0],"Earth",eccen = 0.017)
-d = Planet(typeOforbit[1],s,freq4,[0,0],"Mars",eccen = 0.093)
-e = Planet(typeOforbit[1],s,freq5,[0,0],"Jupiter",eccen = 0.0484)
-f = Planet(typeOforbit[1],s,freq6,[0,0],"Saturn",eccen = 0.054)
 #Mars duration
 #Resolution; the more, the longer it takes! 
 #nsteps = 1100
@@ -240,13 +166,14 @@ f = Planet(typeOforbit[1],s,freq6,[0,0],"Saturn",eccen = 0.054)
 #duration = 7
 
 #Jupiter duration
-#nsteps = 1100
-#duration = 24
+nsteps = 1100
+duration = 24
 
 #Saturn duration
-nsteps = 600
-duration = 30
+#nsteps = 600
+#duration = 30
 
-ss = System([a,b,c,d,e,f],s)
-save = "Saturn-Jupiter"
-Plotting(ss,nsteps,duration,connectingPlanets=[e,f],saveFile=save)
+ss = st.System(name='solar system')
+planets = ss.planets
+
+Plotting(ss,nsteps,duration,connectingPlanets=[planets[2],planets[4]],ignorePlanet=[planets[4],planets[5],planets[6],planets[7]],animationSave=True)
---
+++ starSystems.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+G = 1
+
+class Planet:
+    def __init__(self,typeOfOrbit,star,frequency,initCond,name,eccen=None):
+        self.typeOfOrbit = typeOfOrbit
+        self.star = star
+        self.frequency = frequency
+        self.name = name
+        if (typeOfOrbit == 'circular'):
+            self.eccen = 0
+        elif (typeOfOrbit == 'elliptical'):
+            if (eccen==None):
+                self.eccen = random()
+            else: 
+                self.eccen = eccen
+        self.r = self.findR()
+        self.initCond = initCond
+    
+
+    def __str__(self):
+        return self.name
+
+    def findR(self):
+        """
+        Function that calculates the radius given the type of orbit. This method only 
+        accepts circular and elliptical type at the moment. If the orbit is elliptical
+        it returns two parameters; the semi-major axis and the semi-minor axis.
+        """
+        T = 1/self.frequency
+        if (self.typeOfOrbit == 'circular'):
+            return (T**2*G*self.star.mass/4*np.pi**2)**(1/3)
+        elif (self.typeOfOrbit == 'elliptical'):
+            return [(T**2*G*self.star.mass/4*np.pi**2)**(1/3), np.sqrt((T**2*G*self.star.mass/4*np.pi**2)**(2/3)*(1-self.eccen**2))]
+        return [0,0]
+
+    def orbit(self,t):   
+        """
+        Returns the orbit position as a function of time
+        """ 
+        if (self.typeOfOrbit == 'circular'):
+            return [self.r*np.cos(2*np.pi*self.frequency*t+self.initCond[0]), self.r*np.sin(2*np.pi*self.frequency*t+self.initCond[1])]
+        elif (self.typeOfOrbit == 'elliptical'):
+            return [self.r[0]*(1-self.eccen**2)/(1+self.eccen*np.cos(2*np.pi*self.frequency*t+self.initCond[0]))*np.cos(2*np.pi*self.frequency*t+self.initCond[0]),
+                            self.r[0]*(1-self.eccen**2)/(1+self.eccen*np.cos(2*np.pi*self.frequency*t+self.initCond[1]))*np.sin(2*np.pi*self.frequency*t+self.initCond[1])]
+    def drawOrbit(self):
+        time = np.linspace(0,1/self.frequency,300)
+        xdata, ydata = [], []
+        for t in time: 
+            xdata.append(self.orbit(t)[0])
+            ydata.append(self.orbit(t)[1])
+        return xdata,ydata
+
+class Star: 
+    def __init__(self,mass,radius,name):
+        self.mass = mass
+        self.radius = radius
+        self.name = name
+
+class System: 
+    def __init__(self,p=None, s=None, name=None):
+        """
+        Input: Array of planet specifications (p)
+               Array of star specifications (s)
+               Name: A specific name of a solar system that exists out there.
+        """
+        if (p!=None and s!=None):
+            self.numPlanets = len(p)
+            self.planets = p
+            self.star = s
+
+        elif (name=='solar system'):
+            #Dont really work in units here, so the sun is going to have a radius 1 and mass 1 
+            self.star = Star(1,1,'Sun')
+            self.numPlanets = 8
+            #Frequencies of planets relative to Earth (which is 1)
+            freq = [4.15,1.62,1,0.53,0.084,0.0344,0.01189,0.00606]   
+            names = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+            eccen = [0.206,0.007,0.017,0.093,0.048,0.056,0.047,0.009,0.248]
+            #Eccentricity values were googled and inputted here. 
+            self.planets = [Planet('elliptical',self.star,freq[i],[0,0],names[i],eccen=eccen[i]) for i in range(self.numPlanets)]
+    
+    def __str__(self):  
+        """
+        String method used when printing each system. This will print it in the form of 
+        Star name: [STAR NAME]. Planets: [LIST OF PLANETS]
+        #TO DO: 
+            - make this print details about each planet. Need to re-implement the planet __str__ method 
+        """
+        string = "Star name: " + self.star.name + ". Planets names:" 
+        for i in range(self.numPlanets):
+            if (i == 0):
+                string+=" "+ self.planets[0].name
+            elif (i==self.numPlanets-1):
+                string+= ", " + self.planets[-1].name
+            else: 
+                string+= ", " + self.planets[i].name
+        return string
